9f16b572cc33379ab48b0bee35d9c9f9114de5cf
Pablov <pablo.garcia@enreda.coop>
2018-03-21 16:43:31 +0100

    version demo

--- data/morris-data.js
+++ data/morris-data.js
@@ -3,113 +3,85 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2016 Q1',
-            gasto1: 2666,
-            gasto2: null,
-            gasto3: 2647
-        }, {
-            period: '2016 Q2',
-            gasto1: 2778,
-            gasto2: 2294,
-            gasto3: 2441
-        }, {
-            period: '2016 Q3',
-            gasto1: 4912,
-            gasto2: 1969,
-            gasto3: 2501
-        }, {
-            period: '2016 Q4',
-            gasto1: 3767,
-            gasto2: 3597,
-            gasto3: 5689
-        }, {
-            period: '2017 Q1',
-            gasto1: 6810,
-            gasto2: 1914,
-            gasto3: 2293
-        }, {
-            period: '2017 Q2',
-            gasto1: 5670,
-            gasto2: 4293,
-            gasto3: 1881
-        }, {
-            period: '2017 Q3',
-            gasto1: 4820,
-            gasto2: 3795,
-            gasto3: 1588
-        }, {
-            period: '2017 Q4',
-            gasto1: 15073,
-            gasto2: 5967,
-            gasto3: 5175
-        }, {
-            period: '2018 Q1',
-            gasto1: 10687,
-            gasto2: 4460,
-            gasto3: 2028
+            period: '2018 T1',
+            operacion1: 2666,
+            operacion2: null,
+            operacion3: 2647
         }, {
             period: '2018 Q2',
-            gasto1: 8432,
-            gasto2: 5713,
-            gasto3: 1791
+            operacion1: 2778,
+            operacion2: 2294,
+            operacion3: 2441
+        }, {
+            period: '2018 Q3',
+            operacion1: 4912,
+            operacion2: 1969,
+            operacion3: 2501
+        }, {
+            period: '2018 Q4',
+            operacion1: 3767,
+            operacion2: 3597,
+            operacion3: 5689
+        }, {
+            period: '2019 Q1',
+            operacion1: 6810,
+            operacion2: 1914,
+            operacion3: 2293
+        }, {
+            period: '2019 Q2',
+            operacion1: 5670,
+            operacion2: 4293,
+            operacion3: 1881
+        }, {
+            period: '2019 Q3',
+            operacion1: 4820,
+            operacion2: 3795,
+            operacion3: 1588
+        }, {
+            period: '2019 Q4',
+            operacion1: 15073,
+            operacion2: 5967,
+            operacion3: 5175
+        }, {
+            period: '2020 Q1',
+            operacion1: 10687,
+            operacion2: 4460,
+            operacion3: 2028
+        }, {
+            period: '2020 Q2',
+            operacion1: 8432,
+            operacion2: 5713,
+            operacion3: 1791
         }],
         xkey: 'period',
-        ykeys: ['gasto1', 'gasto2', 'gasto3'],
-        labels: ['Gasto 1', 'Gasto 2', 'Gasto 3'],
+        ykeys: ['operacion1', 'operacion2', 'operacion3'],
+        labels: ['OP1.2 ', 'OP1.1', 'OP3.1'],
         pointSize: 2,
         hideHover: 'auto',
-        resize: true
-    });
-
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Operación 1",
-            value: 12
-        }, {
-            label: "Operación 2",
-            value: 30
-        }, {
-            label: "Operación 3",
-            value: 20
-        }],
         resize: true
     });
 
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2016',
-            a: 50,
-            b: 40
-        }, {
-            y: '2017',
-            a: 75,
-            b: 65
-        }, {
             y: '2018',
-            a: 100,
-            b: 90
+            a: 40,
+            b: 20,
+            c: 10
+        }, {
+            y: '2019',
+            a: 60,
+            b: 40,
+            c: 20
+        }, {
+            y: '2020',
+            a: 20,
+            b: 80,
+            c: 70
         }],
         xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        ykeys: ['a', 'b', 'c'],
+        labels: ['Contratado', 'Ejecutado', 'Justificado'],
         hideHover: 'auto',
         resize: true
     });
